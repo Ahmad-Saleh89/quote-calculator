7d3960d43c94892c6da694b71e3a5090d72c8cf7
Ahmad-Saleh89 <ahmadsaleh1989@gmail.com>
2020-01-13 09:14:20 -0600

    2 files modified

--- src/app/data/courses.ts
+++ src/app/data/courses.ts
@@ -12,6 +12,94 @@ export const COURSES = [
       {
         name: "Angular",
         price: 300
+      },
+      {
+        name: "React",
+        price: 280
+      },
+      {
+        name: "Vue",
+        price: 260
+      },
+      {
+        name: "PHP",
+        price: 460
+      },
+      {
+        name: "WordPress",
+        price: 360
+      },
+      {
+        name: "MySQL",
+        price: 420
+      }
+    ]
+  },
+  {
+    "Design": [
+      {
+        name: "Graphic Design",
+        price: 200
+      },
+      {
+        name: "User Experience",
+        price: 250
+      },
+      {
+        name: "Animation",
+        price: 300
+      },
+      {
+        name: "Fashion",
+        price: 280
+      },
+      {
+        name: "Interior Design",
+        price: 260
+      },
+      {
+        name: "Game Design",
+        price: 460
+      },
+      {
+        name: "Design Tools",
+        price: 360
+      },
+      {
+        name: "Photoshop",
+        price: 420
+      }
+    ]
+  },
+  {
+    "Marketing": [
+      {
+        name: "Digital Marketing",
+        price: 200
+      },
+      {
+        name: "SEO",
+        price: 250
+      },
+      {
+        name: "Branding",
+        price: 300
+      },
+      {
+        name: "Public Relations",
+        price: 280
+      },
+      {
+        name: "Advertising",
+        price: 260
+      },
+      {
+        name: "Social Media",
+        price: 460
+      },
+      {
+        name: "Product Marketing",
+        price: 360
       }
     ]
   },
@@ -22,8 +110,24 @@ export const COURSES = [
         price: 330
       },
       {
-        name: "Drums",
-        price: 180
+        name: "Music Theory",
+        price: 280
+      },
+      {
+        name: "Music History",
+        price: 350
+      },
+      {
+        name: "Music Compostion",
+        price: 400
+      },
+      {
+        name: "Bass Guitar",
+        price: 320
+      },
+      {
+        name: "Piano",
+        price: 420
       }
     ]
   }
